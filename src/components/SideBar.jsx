5897b70c5a4999035b8f326f702e46bfe942b4ae
jarrospide1 <juanarrospidepons@gmail.com>
2022-04-17 17:54:48 -0700

    Cambio de logo y enlaces en menu lateral

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import image from '../assets/images/logo-DH.png';
+import image from '../assets/images/logo_blanco.svg';
+import { Link, Route, Switch } from "react-router-dom";
 
 function SideBar(){
     return(
@@ -7,14 +8,14 @@ function SideBar(){
             <ul className="navbar-nav bg-gradient-secondary sidebar sidebar-dark accordion" id="accordionSidebar">
                 <a className="sidebar-brand d-flex align-items-center justify-content-center" href="/">
                     <div className="sidebar-brand-icon">
-                        <img className="w-100" src={image} alt="Digital House"/>
+                        <img className="w-100" src={image} alt="Habito"/>
                     </div>
                 </a>
                 <hr className="sidebar-divider my-0"/>
                 <li className="nav-item active">
                     <a className="nav-link" href="/">
                         <i className="fas fa-fw fa-tachometer-alt"></i>
-                        <span>Dashboard - DH movies</span>
+                        <span>Dashboard - Habito</span>
                     </a>
                 </li>
                 <hr className="sidebar-divider"/>
@@ -22,19 +23,19 @@ function SideBar(){
                 <li className="nav-item">
                     <a className="nav-link collapsed" href="/">
                         <i className="fas fa-fw fa-folder"></i>
-                        <span>Pages</span>
+                        <span>Productos</span>
                     </a>
                 </li>
                 <li className="nav-item">
-                    <a className="nav-link" href="/">
+                    <a className="nav-link" href="/Users">
                         <i className="fas fa-fw fa-chart-area"></i>
-                        <span>Charts</span>
+                        <span>tabla de Usuarios</span>
                     </a>
                 </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="/">
+                <li className="nav-item" >
+                    <a className="nav-link" href="/Products">
                         <i className="fas fa-fw fa-table"></i>
-                        <span>Tables</span>
+                        <span>Tabla de productos</span>
                     </a>
                 </li>
                 <hr className="sidebar-divider d-none d-md-block"/>
